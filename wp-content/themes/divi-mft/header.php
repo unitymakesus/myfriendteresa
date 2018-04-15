4812249e2e7a48991a0f63f7c339e431e544e81b
Alisa Herr <alisa@unitymakes.us>
2018-04-15 19:52:23 -0400

    Add Google Tag Manager

--- wp-content/themes/divi-mft/header.php
+++ wp-content/themes/divi-mft/header.php
@@ -13,7 +13,13 @@
 <!--<![endif]-->
 <head>
 	<?php if (!is_user_logged_in()) { ?>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WSCT4DK');</script>
+		<!-- End Google Tag Manager -->
 	<?php } ?>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -37,6 +43,12 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<?php if (!is_user_logged_in()) { ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSCT4DK"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php } ?>
 <a href="#et-main-area" class="screen-reader-text">Skip to content</a>
 <?php
 	$product_tour_enabled = et_builder_is_product_tour_enabled();
